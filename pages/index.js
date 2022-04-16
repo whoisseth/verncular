93add4d891c96ca033a9f64c178c4e14f4e2396c
utkarsh <55540613+whoisseth@users.noreply.github.com>
2022-04-16 08:58:36 +0530

    upadate active state

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,9 @@ import { useState } from 'react'
 
 const Home = ({ data }) => {
   const [time, setTime] = useState(data[0].available)
+  const [dateIndex, setDateIndex] = useState(0)
+  const [slotIndex, setSlotIndex] = useState(null)
+
   console.log(time)
   return (
     <div className="relative">
@@ -13,6 +16,7 @@ const Home = ({ data }) => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/*  */}
       <nav className="flex items-center justify-between py-5 px-8 shadow-md md:shadow-none">
         <img
           src="/images/logo.png"
@@ -25,13 +29,24 @@ const Home = ({ data }) => {
         </div>
       </nav>
       <Sidebar />
-
       <hr className=" hidden md:flex" />
+      {/*  */}
       <main className="md:pl-72">
         <BsArrowLeft className="mx-8  mt-6 -mb-6 hidden text-lg md:flex" />
         <Heading />
-        <SelectDate data={data} time={time} setTime={setTime} />
-        <SelectSlot time={time} />
+        <SelectDate
+          data={data}
+          time={time}
+          setTime={setTime}
+          dateIndex={dateIndex}
+          setDateIndex={setDateIndex}
+          setSlotIndex={setSlotIndex}
+        />
+        <SelectSlot
+          time={time}
+          slotIndex={slotIndex}
+          setSlotIndex={setSlotIndex}
+        />
         <button className="mx-8 mt-6 rounded-md bg-sky-900 px-6 py-2 font-semibold  text-white">
           Proceed to Pay
         </button>
@@ -53,23 +68,35 @@ function Heading() {
     </div>
   )
 }
-function SelectDate({ data, setTime, time }) {
+function SelectDate({
+  data,
+  setTime,
+  time,
+  dateIndex,
+  setDateIndex,
+  setSlotIndex,
+}) {
   console.log(data)
 
   return (
     <div className="mt-6 pl-8">
       <h3 className="text-lg font-semibold">Select Date</h3>
       <div className=" mt-2 flex gap-2  overflow-x-auto">
-        {data.map((data, index,) => (
+        {data.map((data, index) => (
           <div
-            
             key={index}
             onClick={() => {
               setTime(data.available)
+              setDateIndex(index)
+              setSlotIndex(null)
             }}
           >
             <DateBox
-              // className={time && 'bg-sky-800 text-white'}
+              className={
+                dateIndex == index
+                  ? 'bg-sky-800 text-white'
+                  : 'bg-white text-sky-800'
+              }
               time={time}
               day={moment(data.date).format('ddd')}
               date={moment(data.date).format('D')}
@@ -81,36 +108,43 @@ function SelectDate({ data, setTime, time }) {
     </div>
   )
 }
-function DateBox({ day, date, month ,className}) {
+function DateBox({ day, date, month, className }) {
   return (
     <button
       className={`flex  w-12 cursor-pointer flex-col items-center rounded border-2 border-sky-800 px-8 font-semibold  ${className}`}
     >
-      {/* ${'bg-sky-800 text-white'} */}
       <div>{day}</div>
       <div className="font-bold">{date}</div>
       <div>{month}</div>
     </button>
   )
 }
-function SelectSlot({ time }) {
+function SelectSlot({ time, setSlotIndex, slotIndex }) {
   console.log(time[0].hour)
   return (
     <div className=" mt-8 px-8">
       <h3 className="text-lg font-semibold">Select Slot</h3>
       <div className="mt-2 flex gap-2">
         {time.map((time, index) => (
-          <div key={index}>
-            <SlotBtn hours={time.hour} />
+          <div key={index} onClick={() => setSlotIndex(index)}>
+            <SlotBtn
+              hours={time.hour}
+              className={slotIndex == index ? 'bg-sky-800 text-white' : ''}
+            />
           </div>
         ))}
       </div>
     </div>
   )
 }
-function SlotBtn({ hours }) {
+function SlotBtn({ hours, className }) {
   return (
-    <button className="tex rounded-md border-2 border-sky-800 px-3 py-1.5 font-medium uppercase text-sky-800 ">
+    <button
+      className={
+        'tex rounded-md border-2 border-sky-800 px-3 py-1.5 font-medium uppercase  ' +
+        className
+      }
+    >
       {hours} pm - {hours + 1} pm
     </button>
   )
@@ -133,7 +167,7 @@ function Sidebar() {
 }
 export default Home
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await fetch(
     `https://mentorplus.s3.ap-south-1.amazonaws.com/config/availability.json`
   )
